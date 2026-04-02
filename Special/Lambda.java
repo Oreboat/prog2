74138e000b7561da555bf9ddba10e78efe6265bc
Felipe Savella <fsavel1@lsu.edu>
2026-04-01 19:34:32 -0500

    Lambda.java

--- Special/Lambda.java
+++ Special/Lambda.java
@@ -1,13 +1,17 @@
-// Lambda -- Parse tree node strategy for printing the special form lambda
-
 package Special;
 
 import Tree.Node;
+import Tree.Environment;
+import Tree.Closure;
 import Print.Printer;
 
 public class Lambda extends Special {
 
-	public void print(Node t, int n, boolean p) {
-		Printer.printLambda(t, n, p);
-	}
+    public void print(Node t, int n, boolean p) {
+        Printer.printLambda(t, n, p);
+    }
+
+    public Node eval(Node t, Environment env) {
+        return new Closure(t, env);
+    }
 }
