62595edf507546fc7f8f4658ddf5117e778b4fad
Felipe Savella <fsavel1@lsu.edu>
2026-04-01 19:30:34 -0500

    Set.java

--- Special/Set.java
+++ Special/Set.java
@@ -1,13 +1,26 @@
-// Set -- Parse tree node strategy for printing the special form set!
-
 package Special;
 
 import Tree.Node;
+import Tree.Environment;
+import Tree.Nil;
 import Print.Printer;
 
 public class Set extends Special {
 
     public void print(Node t, int n, boolean p) {
         Printer.printSet(t, n, p);
+    }
+
+    public Node eval(Node t, Environment env) {
+        Node variable = t.getCdr().getCar();
+        Node value = t.getCdr().getCdr().getCar().eval(env);
+
+        if (variable.isSymbol()) {
+            env.assign(variable, value);
+        } else {
+            System.err.println("Error: set! requires a symbol");
+        }
+
+        return Nil.getInstance();
     }
 }
