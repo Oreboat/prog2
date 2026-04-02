bda38161e81b656ac37fc88251c4fa79845b2c79
Felipe Savella <fsavel1@lsu.edu>
2026-04-01 19:28:25 -0500

    Regular.java

--- Special/Regular.java
+++ Special/Regular.java
@@ -1,13 +1,28 @@
-// Regular -- Parse tree node stratagy for printing regular lists
-
 package Special;
 
 import Tree.Node;
+import Tree.Environment;
+import Tree.Cons;
+import Tree.Nil;
 import Print.Printer;
 
 public class Regular extends Special {
 
     public void print(Node t, int n, boolean p) {
         Printer.printRegular(t, n, p);
+    }
+
+    public Node eval(Node t, Environment env) {
+        Node function = t.getCar().eval(env);
+        Node args = evalArgs(t.getCdr(), env);
+
+        return function.apply(args);
+    }
+
+    private Node evalArgs(Node args, Environment env) {
+        if (args.isNull()) {
+            return Nil.getInstance();
+        }
+        return new Cons(args.getCar().eval(env), evalArgs(args.getCdr(), env));
     }
 }
