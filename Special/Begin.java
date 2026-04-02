6d0b7f6be68a4b2425bad5516a1e80d7e1119912
Felipe Savella <fsavel1@lsu.edu>
2026-04-01 19:32:24 -0500

    Begin.java

--- Special/Begin.java
+++ Special/Begin.java
@@ -1,13 +1,25 @@
-// Begin -- Parse tree node strategy for printing the special form begin
-
 package Special;
 
 import Tree.Node;
+import Tree.Environment;
+import Tree.Nil;
 import Print.Printer;
 
 public class Begin extends Special {
 
     public void print(Node t, int n, boolean p) {
         Printer.printBegin(t, n, p);
+    }
+
+    public Node eval(Node t, Environment env) {
+        Node res = Nil.getInstance();
+        Node exprs = t.getCdr();
+
+        while (!exprs.isNull()) {
+            res = exprs.getCar().eval(env);
+            exprs = exprs.getCdr();
+        }
+
+        return res;
     }
 }
