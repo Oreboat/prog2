4c264ade21b8fb18d7fdf5e4b04595b1dabba1d6
Felipe Savella <fsavel1@lsu.edu>
2026-04-01 19:35:27 -0500

    Cond.java

--- Special/Cond.java
+++ Special/Cond.java
@@ -1,13 +1,47 @@
-// Cond -- Parse tree node strategy for printing the special form cond
-
 package Special;
 
 import Tree.Node;
+import Tree.Environment;
+import Tree.Nil;
 import Print.Printer;
 
 public class Cond extends Special {
 
     public void print(Node t, int n, boolean p) {
         Printer.printCond(t, n, p);
+    }
+
+    public Node eval(Node t, Environment env) {
+        Node clauses = t.getCdr();
+
+        while (clauses.isPair()) {
+            Node clause = clauses.getCar();
+            Node test = clause.getCar();
+
+            if (test.isSymbol() && test.getName().equalsIgnoreCase("else")) {
+                return evalBody(clause.getCdr(), env);
+            }
+
+            Node val = test.eval(env);
+            if (!(val.isBoolean() && val.getName().equals("#f"))) {
+                Node body = clause.getCdr();
+                if (body.isNull()) {
+                    return val;
+                }
+                return evalBody(body, env);
+            }
+            clauses = clauses.getCdr();
+        }
+
+        return Nil.getInstance();
+    }
+
+    private Node evalBody(Node body, Environment env) {
+        Node res = Nil.getInstance();
+        while (body.isPair()) {
+            res = body.getCar().eval(env);
+            body = body.getCdr();
+        }
+        return res;
     }
 }
